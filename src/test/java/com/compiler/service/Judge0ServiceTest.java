be1e34ae76d057cd195c71c10830d69499dc8b68
Navin kumar <navin1sepkumar@gmail.com>
2025-10-13 12:00:55 +0530

    commit

--- src/test/java/com/compiler/service/Judge0ServiceTest.java
+++ src/test/java/com/compiler/service/Judge0ServiceTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,6 +20,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class Judge0ServiceTest {
 
     @Mock
