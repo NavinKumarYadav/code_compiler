de3222d72626b08ac5f5176aee985d0a783066ee
Navin kumar <navin1sepkumar@gmail.com>
2025-10-17 11:30:36 +0530

    Add project documentation, setup guide, and database migrations

---
+++ src/main/resources/db/migration/V4__Insert_sample_users.sql
@@ -0,0 +1,6 @@
+INSERT IGNORE INTO users (username, email, password, created_at) VALUES
+('Sanjeev', 'sanjeev@gmail.com', '$2a$10$f9IGHEKSF6C.B/LrDQ37nOrD/Jru4TrwJHYFVQFArZKCAoM1C/O/6', NOW()),
+('Sanju', 'sanju@gmail.com', '$2a$10$7M4a4CA0/dx6zHXy89t1ZengMkqc2dDirAtuSYQbpkwMUI4Ej8UtC', NOW()),
+('Aditya', 'aditya@gmail.com', '$2a$10$.i7KAAyspyqJZcN7M2YWneRR72Y2FzhfQc5pfoY886WYZG6e8/4lq', NOW()),
+('Sahil', 'sahil@gmail.com', '$2a$10$HtzjXFHFEiog3kjq/OxNMu8TwFsQeuiXKGRQOIM4jhrP6N1KaSUca', NOW()),
+('Deepak', 'deepak@gmail.com', '$2a$10$/6yRz5DOUlxaAKJIfID9sO.gWYqPNXXFcAcue3KvtBSPw0q7U26G.', NOW());
